5b845738ce56104740bc66946b0f21157b0fe933
suregowtham123 <132137128+suregowtham123@users.noreply.github.com>
2025-09-07 00:07:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -373,7 +373,9 @@ if __name__ == '__main__':
     if not os.path.exists('site.db'):
         with app.app_context():
             db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
 
 
 
